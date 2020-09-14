adbedc2ce4d89609fc191ba98a6514c5f522737a
Louis-Philippe Simard <l.psimard901@gmail.com>
2020-09-14 14:04:22 -0400

    Change 3
    Added CalculatorEngine as a DLL (CalculatorLibrary)

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorLibrary;
+using CalculatorLibrary.SimpleCalculator;
 
 namespace SimpleCalculator
 {
